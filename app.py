e9d9318346e4c491eadb836acc4d6204e14a12fe
myname21-meet <myname21@meet.mit.edu>
2020-01-29 10:36:57 -0500

    personal_proj

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, redirect, url_for, render_template
+from flask import session as login_session
+from database import *
+
+app = Flask(__name__)
+app.secret_key = "MY_SUPER_SECRET_KEY"
+
+
+@app.route('/way_to_love')
+def way_to_love():
+	return render_template("way_to_love.html")
+
+@app.route('/about_us')
+def about():
+	return render_template("about_us.html")
+
+@app.route('/my_story', methods=['GET', 'POST'])
+def story():
+	if request.method == 'GET':
+		return render_template('my_story.html')
+	else:
+		name = request.form['name']
+		time = request.form['time']
+		title = request.form['title']
+		story = request.form['story']
+
+
+		add_story(name, time, title, story)      
+		return redirect('access_stories')
+
+@app.route('/delete_stories')
+def delete():
+	delete_all()
+	return render_template("stories.html")
+
+@app.route('/access_stories', methods=['GET', 'POST'])
+def access():
+	if request.method == 'POST':
+		comments = request.form['comments']
+		name = request.form['name']
+		print(name)
+		print(comments)
+
+		add_comment(name, comments)   
+		return render_template("stories.html", stories=query_all())
+	else:
+		return render_template("stories.html", stories=query_all())
+		
+
+@app.route('/reserches')
+def reserches():
+	return render_template("reserch.html")
+
+@app.route('/home')
+def home():
+	return render_template("home.html") 
+
+if __name__ == '__main__':
+	app.run(debug=True, threaded=False)
---
+++ database.py
@@ -0,0 +1,42 @@
+from model import *
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///database.db')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+def add_story(name, time, title, story):
+    story1 = Story(
+        name=name,
+        time=time,
+        title=title,
+        story=story,
+        comments=""
+        )
+    session.add(story1)
+    session.commit()
+
+def query_all():
+	all_stories = session.query(Story).all()
+	return all_stories
+
+def delete_all():
+	session.query(Story).delete()
+	session.commit()
+
+def add_comment(name, comments):
+	story = session.query(Story).filter_by(name=name).first()
+	story.comments +=  comments +"," 
+	session.commit()
+
+
+
+
+
+
+
